f9a3195c05505f92b6d50b8d29ce8890d0aff937
UmarUU2003 <uuuhp2003@gmail.com>
2023-10-25 08:42:42 +0500

    OOP Task 2 completed.

--- src/models/RightTriangle.java
+++ src/models/RightTriangle.java
@@ -10,7 +10,36 @@ public class RightTriangle {
         this.secondLegLength = secondLegLength;
         hypotenuseLength = Math.sqrt(Math.pow(firstLegLength, 2) + Math.pow(secondLegLength, 2));
     }
+    public static double area(int firstLegLength, int secondLegLength){
+        return (double) (firstLegLength * secondLegLength) / 2;
+    }
+    public static double area(long firstLegLength, long secondLegLength){
+        return (double) (firstLegLength * secondLegLength) / 2;
+    }
 
+    public static double area(float firstLegLength, float secondLegLength){
+        return firstLegLength * secondLegLength / 2;
+    }
+    public static double area(double firstLegLength, double secondLegLength){
+        return firstLegLength * secondLegLength / 2;
+    }
+    public static double perimeter(int firstLegLength, int secondLegLength){
+        double hypotenuseLength = Math.sqrt(Math.pow(firstLegLength, 2) + Math.pow(secondLegLength, 2));
+        return firstLegLength + secondLegLength + hypotenuseLength;
+    }
+    public static double perimeter(long firstLegLength, long secondLegLength){
+        double hypotenuseLength = Math.sqrt(Math.pow(firstLegLength, 2) + Math.pow(secondLegLength, 2));
+        return firstLegLength + secondLegLength + hypotenuseLength;
+    }
+    public static double perimeter(float firstLegLength, float secondLegLength){
+        double hypotenuseLength = Math.sqrt(Math.pow(firstLegLength, 2) + Math.pow(secondLegLength, 2));
+        return firstLegLength + secondLegLength + hypotenuseLength;
+    }
+
+    public static double perimeter(double firstLegLength, double secondLegLength){
+        double hypotenuseLength = Math.sqrt(Math.pow(firstLegLength, 2) + Math.pow(secondLegLength, 2));
+        return firstLegLength + secondLegLength + hypotenuseLength;
+    }
     public double area(){
         return getFirstLegLength() * getSecondLegLength() / 2;
     }
